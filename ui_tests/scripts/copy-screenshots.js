158d0b3bb6c12c2930776482a829b2495df977e1
Paul Patarinski <paulpatarinski@gmail.com>
2017-02-05 15:53:47 -0600

    Clean screenshots

--- ui_tests/scripts/copy-screenshots.js
+++ ui_tests/scripts/copy-screenshots.js
@@ -4,6 +4,6 @@ var copyfiles = require('copyfiles');
 var uiTestsBin = path.join(projRoot, 'ui_tests', 'bin', 'Debug');
 var androidPhoneDest = path.join(projRoot, 'fastlane','metadata','android', 'en-US', 'images', 'phoneScreenshots');
 
-copyfiles([uiTestsBin + "/android-phone-*.png",androidPhoneDest], true, function (er, files) {
+copyfiles([uiTestsBin + "/android-phone*.png",androidPhoneDest], true, function (er, files) {
     console.log(files);
 });
